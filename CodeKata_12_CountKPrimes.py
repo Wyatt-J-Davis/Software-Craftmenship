98f245d31746ded6e8864953a3c38acf38f70ecc
Wyatt Davis <whyit11@gmail.com>
2022-06-26 18:33:39 -0500

    Turn k prime factor finder into a class

--- CodeKata_12_CountKPrimes.py
+++ CodeKata_12_CountKPrimes.py
@@ -1,7 +1,8 @@
 import utils.KPrimes as KPrimes
 
 def test_findKPrimes():
-    primesList = KPrimes.findKPrimes(5, 500, 600)
+    kPrimeFinder = KPrimes.KPrimeFactorFinder()
+    primesList = kPrimeFinder.findKPrimes(5, 500, 600)
     assert primesList == [500, 520, 552, 567, 588, 592, 594]
 
 test_findKPrimes()
--- utils/KPrimes.py
+++ utils/KPrimes.py
@@ -1,20 +1,22 @@
-def findKPrimes(k, start, end):
-    kPrimes = []
-    for i in range(start, end, 1):
-        primeFactors = prime_factors(i)
-        if(len(primeFactors) == k):
-            kPrimes.append(i)
-    return kPrimes
-             
-def prime_factors(n):
-    i = 2
-    factors = []
-    while i*i <= n:
-        if n % i:
-            i += 1
+class KPrimeFactorFinder(object):
+    
+    def findKPrimes(self, k, start, end):
+        kPrimes = []
+        for i in range(start, end, 1):
+            primeFactors = self._prime_factors(i)
+            if(len(primeFactors) == k):
+                kPrimes.append(i)
+        return kPrimes
+    
+    def _prime_factors(self,n):
+        i = 2
+        factors = []
+        while i*i <= n:
+            if n % i:
+                i += 1
         else:
             n //= i
             factors.append(i)
-    if n > 1:
-        factors.append(n)
-    return factors
+        if n > 1:
+            factors.append(n)
+        return factors
